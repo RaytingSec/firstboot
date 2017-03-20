281faef467158774b7ecb5730e5e35b13a28de34
raytingsec <rayting@raydar.net>
2017-03-20 05:09:14 -0700

    fix: corrected sublime package manager url, partially moved to new config dir

--- configure.sh
+++ configure.sh
@@ -13,11 +13,12 @@ mkdir -p ~/.vim/colors
 mv $CONFIGS/molokai.vim ~/.vim/colors
 
 # cowsay
-sudo cp $CONFIGS/fox.cow /usr/share/cowsay/cows/
+sudo cp $CONFIGS/rayting.cow /usr/share/cowsay/cows/
 
 # inputrc
-printf '\nset completion-ignore-case On' | sudo -E tee /etc/inputrc > /dev/null
+# printf '\nset completion-ignore-case On' | sudo -E tee /etc/inputrc > /dev/null
 
 # sublime
-wget -P ~/.config/sublime-text-3/Installed\ Packages https://sublime.wbond.net/Package%20Control.sublime-package
-cp $CONFIGS/sublime/* ~/.config/sublime-text-3/Packages/User/
+CONFIGS=~/code/sublime-config
+wget --directory-prefix="~/.config/sublime-text-3/Installed Packages" https://packagecontrol.io/Package%20Control.sublime-package
+cp $CONFIGS/* ~/.config/sublime-text-3/Packages/User/
