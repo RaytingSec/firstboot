0ab7661edd1a2f1857677edaf8f2809a206fd743
raytingsec <rayting@raydar.net>
2017-03-20 05:10:54 -0700

    fix: updated dir names, removed some trailing forward slashes

--- directories.sh
+++ directories.sh
@@ -23,15 +23,15 @@ elif [[ "$1" == "--torrents" ]]; then
     mkdir -p $DATA/Torrent/srcload
 elif [[ "$1" == "--symlinks" ]]; then
     # Symlinks
-    ln -s $DATA/vm/ ~/vmware
+    ln -s $DATA/vm ~/vmware
     ln -s $DATA/dev ~/dev
     ln -s $DATA/Dropbox ~/Dropbox
-    ln -s $DATA/Code/ ~/code
-    ln -s ~/code/Scripts/Bash/ ~/bin
+    ln -s $DATA/Code ~/code
+    ln -s ~/code/Scripts/Bash ~/bin
     ln -s ~/code/Scripts/Python ~/py
 
     ln -s ~/code/cryptopals ~/crypto
-    ln -s ~/code/Stockfighter ~/stock
+    ln -s ~/code/stockfighter ~/stock
 
     ln -s ~/Dropbox/Documents/SJSU/Courses ~/courses
 fi
