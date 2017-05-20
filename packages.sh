cdbbcfc0f01ceb28e86d9d5fe85d0fc886567a4c
raytingsec <rayting@raydar.net>
2017-05-20 15:30:20 -0700

    rem: deleted lots of packages to test basic functionality

--- packages.sh
+++ packages.sh
@@ -28,234 +28,151 @@ while true; do
     esac
 done
 
-# Logic flow
-# base
-# host?
-#     yes
-#     no
-# vm?
-#     yes
-# server?
-#     yes (server)
-#     no (dev, uses display)
-
-# Packages
-
-url_chrome="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-url_slack=$(curl -sS https://slack.com/downloads/instructions/linux | grep amd64 | cut -d \" -f 4)
-url_sublime=$(curl -sS https://www.sublimetext.com/ | grep download | grep amd64 | cut -d \" -f 4)
-
-packages_base=(
-    # Essentials
-    "vim "
-    "tmux "
-    "tree "
-    "git "
-    "colordiff "
-    "software-properties-common "
-    "ppa-purge "
-    # Sys monitor
-    "htop "
-    "jnettop "
-    "iotop "
-    # Cowsay stuff
-    "fortunes "
-    "cowsay "
-    # Network stuff
-    "sshfs "
-    "openvpn "
-    "nmap "
-    "curl "
-)
-packages_base_extra=(
-    "whois "
-    "atop "
-    "finger "
-    "hping3 "
-    # Fortunes
-    "fortunes-debian-hints "
-    "fortunes-spam "
-    "fortunes-ubuntu-server "
-    # For fun
-    "bsdgames "
-    "bsdgames-nonfree "
-)
-
-packages_host=(
-    "cpufreqinfo "
-    "tlp "
-    "pm-utils "
-    "lm-sensors "
-    "nautilus-dropbox "
-    "network-manager-openvpn-gnome "
-    "youtube-dl "
-)
-packages_host_urls=(
-    $url_slack
-)
-packages_host_extra=(
-    "banshee "
-)
-packages_not_host=(
-    "ssh "
-)
-
-# packages_vm=(
-#     "open-vm-tools-desktop "
-#     "open-vm-tools-dkms "
-# )
-
-packages_server=(
-    "iptstate "
-    # "nginx "
-)
-packages_server_sql=(
-    "mysql-server "
-)
-packages_server_torrent=(
-    "transmission-cli "
-    "transmission-daemon "
-    "transmission-common "
-)
-packages_server_torrent_ppa=(
-    "ppa:transmissionbt/ppa"
-)
-
-packages_gui=(
-    "fonts-hack-otf "
-    "simplescreenrecorder "
-    "vlc "
-    # Chrome dependencies
-    "libappindicator1 "
-    "libindicator7 "
-)
-packages_gui_url=(
-    $url_chrome
-)
-
-packages_dev=(
-    # Dev tools
-    "httpie "
-    "oracle-java8-installer "
-    "oracle-java8-set-default "
-    "python3-pip "
-    # Security
-    "sox "
-    "libsox-fmt-mp3 "
-    "radare2 "
-    "scanmem "
-    "hexedit "
-    "nikto "
-    "hydra "
-    "sqlmap "
-    "wireshark "
-    "masscan "
-    "zmap "
-)
-packages_dev_ppa=(
-    "ppa:webupd8team/java"
-)
-packages_dev_url=(
-    $url_sublime
-)
-packages_dev_pip=(
-    # "pip "
-    "virtualenv "
-    "setuptools "
-    "bs4 "
-    "markdown "
-    "requests "
-    "crypto "
-    "numpy "
-    "matplotlib "
-    "requests[security]"
-    # keyczar
-    # nacl
-)
-packages_dev_extra=(
-    #plugins
-    "icedtea-plugin "
-    "icedtea-8-plugin "
-    "flashplugin-installer "
-    #other
-    "nodejs "
-    "npm "
-    "node-typescript "
-    "brackets "
-    "sqlitebrowser "
-    "gns3-gui "
-    "gns3-iou "
-)
-packages_dev_extra_ppa=(
-    "ppa:webupd8team/brackets"
-    "ppa:gns3/ppa"
-)
-
-# Testing stuff
-packages_testing=(
-    "backuppc "
-    "bacula "
-    "byobu "
-)
-
-# Logic flow
-# base
-# host?
-#     yes
-#     no
-# vm?
-#     yes
-# server?
-#     yes (server)
-#     no (dev, uses display)
-
 repos=()
 packages=()
 packages_pip=()
 urls=()
 
-packages+=$packages_base
-packages+=$packages_base_extra
+url_chrome="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+url_slack=$(curl -sS https://slack.com/downloads/instructions/linux | grep amd64 | cut -d \" -f 4)
+url_sublime=$(curl -sS https://www.sublimetext.com/ | grep download | grep amd64 | cut -d \" -f 4)
 
-echo "configuring for install..."
+# packages_base=(
+packages+=(
+    # Essentials
+    "vim"
+    "tmux"
+    "tree"
+    "git"
+    "colordiff"
+    "software-properties-common "
+    "ppa-purge "
+    # Sys monitor
+    "htop"
+    "jnettop"
+    "iotop"
+    # Cowsay stuff
+    "fortunes"
+    "cowsay"
+    # Network stuff
+    "sshfs"
+    "openvpn"
+    "nmap"
+    "curl"
+)
+# packages_base_extra=(
+packages+=(
+    "whois"
+    "atop"
+    "finger"
+    "hping3"
+    # Fortunes
+    "fortunes-debian-hints "
+    "fortunes-spam "
+    "fortunes-ubuntu-server "
+    # For fun
+    "bsdgames"
+    "bsdgames-nonfree "
+)
+
 if $host; then
-    echo "adding host packages"
-    packages+=$packages_host
-    packages+=$packages_host_urls
-    # packages+=$packages_host_extra
+    packages+=(
+        "cpufreqinfo"
+        "tlp"
+        "pm-utils "
+        "lm-sensors "
+        "nautilus-dropbox "
+        "network-manager-openvpn-gnome "
+        "youtube-dl "
+    )
+    urls+=(
+        $url_slack
+    )
 else
-    packages+=$packages_not_host
+    packages+=(
+        "ssh"
+    )
 fi
 
-# Done in drivers.sh
-# if $vm; then
-#     echo "adding vm packages"
-#     packages+=$packages_vm
-# fi
 
 if $server; then
-    echo "adding server packages"
-    packages+=$packages_server
-    # packages+=$packages_server_sql
-    # packages+=$packages_server_torrent_ppa
+    packages+=(
+        "iptstate"
+    )
 else
-    packages+=$packages_gui
-    urls+=$packages_gui_url
-
-    packages+=$packages_dev
-    repos+=$packages_dev_ppa
-    urls+=$packages_dev_url
-    packages_pip+=$packages_dev_pip
-    # packages+=$packages_dev_extra
-    # packages+=$packages_dev_extra_ppa
+    packages_gui=(
+        "fonts-hack-otf "
+        "simplescreenrecorder"
+        "vlc"
+        # Chrome dependencies
+        "libappindicator1"
+        "libindicator7"
+    )
+    urls+=(
+        $url_chrome
+    )
 fi
 
-selected_repos=" "
+packages+=(
+    # Dev tools
+    "httpie"
+    "python3-pip "
+    # Security
+    "sox"
+    "libsox-fmt-mp3 "
+    "radare2"
+    "scanmem"
+    "hexedit"
+    "nikto"
+    "hydra"
+    "sqlmap"
+    "wireshark"
+    "masscan"
+    "zmap"
+)
+packages_pip+=(
+    # "pip"
+    "virtualenv"
+    "setuptools"
+    "bs4"
+    "markdown"
+    "requests"
+    "crypto"
+    "numpy"
+    "matplotlib"
+    "requests[security]"
+    # keyczar
+    # nacl
+)
+urls+=(
+    $url_sublime
+)
+
+# Logic flow
+# base
+# host?
+#     yes
+#     no
+# vm?
+#     yes
+# server?
+#     yes (server)
+#     no (dev, uses display)
+
+# repos=()
+# packages=()
+# packages_pip=()
+# urls=()
+
+# packages+=$packages_base
+# packages+=$packages_base_extra
+
+selected_repos=""
 for r in "${repos[@]}"; do
     selected_repos+="sudo add-apt-repository -y $r; "
 done
 
-selected_packages=" "
+selected_packages=""
 for p in "${packages[@]}"; do
     selected_packages+=$p" "
 done
@@ -267,7 +184,7 @@ for p in "${packages_pip[@]}"; do
     selected_pip+=$p" "
 done
 
-selected_urls=" "
+selected_urls=""
 for u in "${urls[@]}"; do
     selected_urls+="wget $u; "
 done
@@ -288,4 +205,5 @@ elif $print; then
 elif $update; then
     echo "updating existing packages..."
     echo "sudo apt update && sudo apt upgrade"
+    # sudo apt update && sudo apt upgrade -y
 fi
