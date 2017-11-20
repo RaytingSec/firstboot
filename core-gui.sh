8b6e98d586799223886b76763d057eeff6f2f0e5
raytingsec <rayting@raydar.net>
2017-11-19 20:05:59 -0800

    add: remove splashscreen boot

---
+++ core-gui.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# substitute change line in /etc/default/grub
+# GRUB_CMDLINE_LINUX_DEFAULT=""
+# sudo ...
+# sed "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/g"
+sudo sed -i "/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"\"" /etc/default/grub
+sudo update-grub
