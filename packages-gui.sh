0c063e8fa9c2be1ae09a2ce9c132ca0855b9fc21
raytingsec <rayting@raydar.net>
2017-11-19 18:49:50 -0800

    fix: removed bloat from guests

--- packages-gui.sh
+++ packages-gui.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
-sudo add-apt-repository -y ppa:phoerious/keepassxc
-sudo add-apt-repository -y ppa:mozillateam/firefox-next
-sudo apt update
-
 packages=(
     "fonts-hack-otf"
-    # "vlc"
-    "simplescreenrecorder"
-    "keepassxc"
-    "firefox"
-    "chromium-browser"
 )
 selected_packages=""
 for p in "${packages[@]}"; do
     selected_packages+=$p" "
 done
 sudo apt install -y $selected_packages
-
-# VLC config
-cp /tmp/configs/linux-config/vlc-qt-interface.conf ~/.config/vlc
--- packages-host.sh
+++ packages-host.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
+sudo add-apt-repository -y ppa:phoerious/keepassxc
+sudo add-apt-repository -y ppa:mozillateam/firefox-next
+sudo apt update
+
 packages=(
+    "simplescreenrecorder"
+    "keepassxc"
+    "firefox"
+    "chromium-browser"
     "vlc"
     "clementine"
     "bsdgames"
@@ -19,6 +28,9 @@ for p in "${packages[@]}"; do
     selected_packages+=$p" "
 done
 sudo apt install -y $selected_packages
+
+# VLC config
+cp /tmp/configs/linux-config/vlc-qt-interface.conf ~/.config/vlc
 
 packages_pip+=(
     "setuptools"
