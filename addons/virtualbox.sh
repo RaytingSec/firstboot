8d4ab76fd95171336baabcf49aee3614c1057684
raytingsec <rayting@raydar.net>
2018-04-27 03:56:22 -0700

    fix: virtualbox source

--- addons/virtualbox.sh
+++ addons/virtualbox.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "deb https://download.virtualbox.org/virtualbox/debian <mydist> contrib" | sudo tee -a /etc/apt/sources.list
+
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 sudo apt update
 sudo apt install virtualbox-5.2
@@ -10,3 +13,5 @@ sudo apt install virtualbox-5.2
 vboxmanage natnetwork add --netname vboxnat1 --network "192.168.60.0/24" --enable --dhcp on
 ln -s ./Data/vbox "VirtualBox VMs"
 # Copy config files
+
+# Add user to group
