0983a330061db30d2d2dd0baffbc2f17c677c1fb
raytingsec <rayting@raydar.net>
2019-05-29 19:20:47 -0700

    fix: updated extension config

--- extensions-host.sh
+++ extensions-host.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Extensions
+# https://extensions.gnome.org/extension/818/battery-percentage/
 # https://extensions.gnome.org/extension/55/media-player-indicator/
-# https://extensions.gnome.org/extension/1180/freon/
+# https://extensions.gnome.org/extension/946/panel-world-clock-lite/
+# https://extensions.gnome.org/extension/1206/clock-override/
+# https://extensions.gnome.org/extension/841/freon/
 
 ext_path="/org/gnome/shell/extensions"
 
@@ -15,30 +18,13 @@ dconf write $ext_path/$ext_name/status-size 300
 
 ext_name="freon"
 dconf write $ext_path/$ext_name/show-decimal-value false
-dconf write $ext_path/$ext_name/hot-sensors "['Core 0', 'fan1']"
+dconf write $ext_path/$ext_name/hot-sensors "['__average__', 'fan1']"
+dconf write $ext_path/$ext_name/gpu-utility "'nvidia-settings'"
+dconf write $ext_path/$ext_name/position-in-panel "'right'"
+dconf write $ext_path/$ext_name/show-voltage true
 dconf write $ext_path/$ext_name/update-time 5
 
-# ext_name="dynamic-panel-transparency"
-# dconf write $ext_path/$ext_name/theme-opacity 241
-# dconf write $ext_path/$ext_name/enable-opacity false
-# dconf write $ext_path/$ext_name/remove-panel-styling false
-# dconf write $ext_path/$ext_name/transition-with-overview false
-# dconf write $ext_path/$ext_name/maximized-opacity 255
-# dconf write $ext_path/$ext_name/force-theme-update false
-# dconf write $ext_path/$ext_name/enable-background-color false
-# # dconf write $ext_path/$ext_name/panel-theme-color (44, 44, 44)
-# dconf write $ext_path/$ext_name/hide-corners true
-# # dconf write $ext_path/$ext_name/app-overrides @as []
-# dconf write $ext_path/$ext_name/current-user-theme "'Arc-Grey-Dark'"
-# # dconf write $ext_path/$ext_name/text-shadow-position (0, 2, 5)
-# dconf write $ext_path/$ext_name/transition-type 1
-# dconf write $ext_path/$ext_name/transition-speed 400
-# dconf write $ext_path/$ext_name/text-shadow true
-# dconf write $ext_path/$ext_name/icon-shadow true
-# # dconf write $ext_path/$ext_name/icon-shadow-position (0, 2, 5)
-# dconf write $ext_path/$ext_name/trigger-apps @as []
-# dconf write $ext_path/$ext_name/force-animation false
-
-dconf write /org/gnome/shell/enabled-extensions "['alternate-tab@gnome-shell-extensions.gcampax.github.com', 'battery-percentage@nohales.org', 'dash-to-dock@micxgx.gmail.com', 'freon@Veske', 'mediaplayer@patapon.info', 'clipboard-indicator@tudmotu.com', 'middleclickclose@paolo.tranquilli.gmail.com', 'remove-dropdown-arrows@mpdeimos.com', 'windowoverlay-icons@sustmidown.centrum.cz', 'user-theme@gnome-shell-extensions.gcampax.github.com', 'impatience@gfxmonk.net', 'topIcons@adel.gadllah@gmail.com', 'drive-menu@gnome-shell-extensions.gcampax.github.com']"
+# dconf write /org/gnome/shell/enabled-extensions "['dash-to-dock@micxgx.gmail.com', 'alternate-tab@gnome-shell-extensions.gcampax.github.com', 'user-theme@gnome-shell-extensions.gcampax.github.com', 'drive-menu@gnome-shell-extensions.gcampax.github.com', 'TopIcons@phocean.net', 'System_Monitor@bghome.gmail.com', 'workspace-indicator@gnome-shell-extensions.gcampax.github.com', 'apps-menu@gnome-shell-extensions.gcampax.github.com', 'freon@UshakovVasilii_Github.yahoo.com', 'mediaplayer@patapon.info', 'windowoverlay-icons@sustmidown.centrum.cz', 'middleclickclose@paolo.tranquilli.gmail.com', 'world_clock@ailin.nemui', 'world_clock_lite@ailin.nemui', 'impatience@gfxmonk.net', 'remove-dropdown-arrows@mpdeimos.com']"
 
 sudo dconf update
+
--- extensions.sh
+++ extensions.sh
@@ -3,9 +3,6 @@
 # dconf dump /org/gnome/shell/extensions/
 
 # Extensions
-# https://extensions.gnome.org/extension/15/alternatetab/
-# https://extensions.gnome.org/extension/818/battery-percentage/
-# https://extensions.gnome.org/extension/779/clipboard-indicator/
 # https://extensions.gnome.org/extension/277/impatience/
 # https://extensions.gnome.org/extension/307/dash-to-dock/
 # https://extensions.gnome.org/extension/352/middle-click-to-close-in-overview/
@@ -14,11 +11,11 @@
 
 ext_path="/org/gnome/shell/extensions"
 
-ext_name="clipboard-indicator"
-dconf write $ext_path/$ext_name/cache-disable true
-dconf write $ext_path/$ext_name/enable-keybindings false
-dconf write $ext_path/$ext_name/notify-on-copy false
-dconf write $ext_path/$ext_name/preview-size 40
+# ext_name="clipboard-indicator"
+# dconf write $ext_path/$ext_name/cache-disable true
+# dconf write $ext_path/$ext_name/enable-keybindings false
+# dconf write $ext_path/$ext_name/notify-on-copy false
+# dconf write $ext_path/$ext_name/preview-size 40
 
 ext_name="windowoverlay-icons"
 dconf write $ext_path/$ext_name/icon-size 48
@@ -34,29 +31,47 @@ ext_name="net/gfxmonk/impatience"
 dconf write $ext_path/$ext_name/speed-factor 0.75
 
 ext_name="dash-to-dock"
+dconf write $ext_path/$ext_name/apply-custom-theme false
+dconf write $ext_path/$ext_name/autohide true
+dconf write $ext_path/$ext_name/background-opacity 0.69999999999999996
+dconf write $ext_path/$ext_name/click-action "'cycle-windows'"
+dconf write $ext_path/$ext_name/custom-background-color false
+dconf write $ext_path/$ext_name/custom-theme-customize-running-dots true
+dconf write $ext_path/$ext_name/custom-theme-running-dots true
+dconf write $ext_path/$ext_name/custom-theme-running-dots-border-color "'#ffffff'"
+dconf write $ext_path/$ext_name/custom-theme-running-dots-border-width 0
+dconf write $ext_path/$ext_name/custom-theme-running-dots-color "'#0993d9'"
+dconf write $ext_path/$ext_name/custom-theme-shrink true
+dconf write $ext_path/$ext_name/dash-max-icon-size 48
+dconf write $ext_path/$ext_name/dock-fixed false
+dconf write $ext_path/$ext_name/dock-position "'LEFT'"
+dconf write $ext_path/$ext_name/extend-height false
+dconf write $ext_path/$ext_name/force-straight-corner false
+dconf write $ext_path/$ext_name/height-fraction 1.0
+dconf write $ext_path/$ext_name/intellihide-mode "'ALL_WINDOWS'"
+dconf write $ext_path/$ext_name/isolate-workspaces false
+dconf write $ext_path/$ext_name/middle-click-action "'quit'"
+dconf write $ext_path/$ext_name/multi-monitor false
+dconf write $ext_path/$ext_name/opaque-background true
 dconf write $ext_path/$ext_name/preferred-monitor 0
+dconf write $ext_path/$ext_name/pressure-threshold 50.0
+dconf write $ext_path/$ext_name/require-pressure-to-show true
+dconf write $ext_path/$ext_name/running-indicator-dominant-color false
+dconf write $ext_path/$ext_name/running-indicator-style "'SEGMENTED'"
+dconf write $ext_path/$ext_name/scroll-action "'switch-workspace'"
 dconf write $ext_path/$ext_name/shift-click-action "'launch'"
 dconf write $ext_path/$ext_name/shift-middle-click-action "'launch'"
-dconf write $ext_path/$ext_name/custom-theme-running-dots-color "'#00aaff'"
-dconf write $ext_path/$ext_name/click-action "'cycle-windows'"
-dconf write $ext_path/$ext_name/force-straight-corner false
+dconf write $ext_path/$ext_name/show-favorites true
+dconf write $ext_path/$ext_name/show-running true
 dconf write $ext_path/$ext_name/show-show-apps-button false
-dconf write $ext_path/$ext_name/intellihide-mode "'ALL_WINDOWS'"
-dconf write $ext_path/$ext_name/multi-monitor false
-dconf write $ext_path/$ext_name/scroll-action "'switch-workspace'"
-dconf write $ext_path/$ext_name/custom-theme-customize-running-dots true
-dconf write $ext_path/$ext_name/apply-custom-theme false
-dconf write $ext_path/$ext_name/custom-theme-running-dots true
+dconf write $ext_path/$ext_name/transparency-mode "'FIXED'"
 dconf write $ext_path/$ext_name/unity-backlit-items false
-dconf write $ext_path/$ext_name/dock-position "'LEFT'"
-dconf write $ext_path/$ext_name/custom-theme-shrink false
-dconf write $ext_path/$ext_name/background-opacity 0.8
-dconf write $ext_path/$ext_name/dash-max-icon-size 48
-dconf write $ext_path/$ext_name/middle-click-action "'quit'"
-dconf write $ext_path/$ext_name/opaque-background true
 
-ext_name="user-theme"
-dconf write $ext_path/$ext_name/name "'Arc-Grey-Dark'"
+ext_name="topicons"
+dconf write $ext_path/$ext_name/icon-spacing 12
+dconf write $ext_path/$ext_name/tray-pos "'right'"
+dconf write $ext_path/$ext_name/icon-saturation 0.0
+dconf write $ext_path/$ext_name/tray-order 6
 
 dconf write /org/gnome/shell/enabled-extensions "['alternate-tab@gnome-shell-extensions.gcampax.github.com', 'battery-percentage@nohales.org', 'dash-to-dock@micxgx.gmail.com', 'windowoverlay-icons@sustmidown.centrum.cz', 'user-theme@gnome-shell-extensions.gcampax.github.com','impatience@gfxmonk.net', 'drive-menu@gnome-shell-extensions.gcampax.github.com', 'TopIcons@phocean.net']"
 
