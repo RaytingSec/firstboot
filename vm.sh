dafc2afd3f9f5351806d87500a5072c97bb39295
raytingsec <rayting@raydar.net>
2019-05-29 19:45:15 -0700

    fix: changes to how VMs are handled

--- vm.sh
+++ vm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # VMware (doesn't seem to be needed)
-# open-vm-tools-{desktop,dkms}
-# sudo apt install -y open-vm-tools open-vm-tools-desktop open-vm-tools-dkms
+# sudo apt install -y open-vm-tools-{desktop,dkms}  # dkms no longer available
+# sudo apt install -y open-vm-tools-desktop
 
 # VirtualBox (seems to break it though)
-# virtualbox-guest-{x11,dkms,utils}
-
+sudo apt install -y virtualbox-guest-{x11,dkms,utils}
+sudo usermod -aG vboxsf $(whoami)
 # sudo apt install -y virtualbox-guest-dkms
--- vmware.sh
+++ vmware.sh
@@ -3,7 +3,9 @@
 # Installation
 ./VMware-Workstation-Full-12.5.0-4352439.x86_64.bundle
 # append `NoDisplay=true` to /usr/share/applications/ vmware-player and vmware-netcfg desktop files
-echo "NoDisplay=true" | sudo tee -a /usr/share/applications/vmware-{player,netcfg}.desktop
+echo "NoDisplay=true" | sudo tee -a /usr/share/applications/vmware-player.desktop  # Could remove vmware-netcfg too
+# /usr/local doesn't seem to need replacing
+#echo "NoDisplay=true" | sudo tee -a /usr/local/share/applications/vmware-{player,netcfg}.desktop
 
 # Configure
 vmware
@@ -12,5 +14,6 @@ echo "mks.gl.allowBlacklistedDrivers = TRUE" >> ~/.vmware/preferences
 cd ~/vmware
 find -name "*.vmx" -exec vmware {} \;
 
-./thinkorswim_installer.sh
-thinkorswim && rm -r ~/.com.devexperts.tos.ui.user.login.ThinkOrSwimApplication/ && exit
+# Fedora
+# bug where ovftool can't run, causing .ova files to fail to import, needs some lib files
+sudo dnf install libnsl libxcrypt-compat -y
