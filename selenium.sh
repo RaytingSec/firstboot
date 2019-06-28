361280173e2c38a954d9c66c9472d20b8e52c661
raytingsec <rayting@raydar.net>
2019-06-27 23:35:08 -0700

    add: selenium installer
    Installs selenium via pip. Sets up firefox gecko driver.

---
+++ selenium.sh
@@ -0,0 +1,7 @@
+pip install selenium
+
+# Get the latest release from https://github.com/mozilla/geckodriver/releases
+# url=https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+wget -qO- https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz | tar -xz
+sudo mv geckodriver /usr/local/bin/
+sudo chown root:root /usr/local/bin/geckodriver
