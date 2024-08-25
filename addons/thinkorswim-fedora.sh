034bb12cfa2c59b2aee46c796745d5e701e4fa27
raytingsec <rayting@raydar.net>
2024-08-25 16:50:51 -0700

    update to zulu22

--- addons/thinkorswim-fedora.sh
+++ addons/thinkorswim-fedora.sh
@@ -5,15 +5,27 @@
 
 wget https://cdn.azul.com/zulu/bin/zulu-repo-1.0.0-1.noarch.rpm
 sudo dnf install -y zulu-repo-1.0.0-1.noarch.rpm
-sudo dnf install -y zulu-11
-# sudo update-alternatives --config java
-sudo update-alternatives --set java /usr/lib/jvm/zulu11/bin/java
+sudo dnf install -y zulu17
+# Set interactively
+# sudo alternatives --config java
+# Can confirm path with:
+# alternatives --display java | grep '/bin/java' | grep zulu
+sudo alternatives --set java /usr/lib/jvm/java-17-zulu-openjdk/bin/java
 
 # Install Thinkorswim itself
 
 wget https://mediaserver.thinkorswim.com/installer/InstFiles/thinkorswim_installer.sh
 chmod +x thinkorswim_installer.sh
-./thinkorswim_installer.sh  # Accept terms and conditions, use default options
+./thinkorswim_installer.sh -c  # CLI mode
+# Accept terms and conditions, use default options:
+# lang: enter
+# install: enter
+# account: enter
+# license: enter or 2
+# user: enter
+# folder: enter
+# desktop integration: enter
+# run: enter
 
 # Configuration
 
